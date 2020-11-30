0cd74f07d2248f19e0aefd492a4abf425a47fde6
Sujan <sujan.maharjan75@gmail.com>
2020-11-30 11:54:50 +0545

    State

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,69 @@
 import React,{Component} from 'react';
 import './home.css';
 class HomePage extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            number:2,
+            fullname:"",
+            email:"",
+            postcode:""
+        }
+        // this.incrementNumber=this.incrementNumber.bind(this);
+    }
+    incrementNumber=()=> {
+        this.setState({
+            number: ++this.state.number
+        });
+    }
+    decrementNumber=()=> {
+        this.setState({
+            number:  --this.state.number
+        });
+    }
+    handleInputChange=(e)=>{
+        this.setState({
+            number: e.target.value
+        })
+    }
+
+    handleFormInputChange= (e) =>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
+
    render(){
        return (
             <div>
-                <h1 className="heading">This is homepage</h1> 
-                <p>This is a short description</p>
-                <p>multiple paragraph</p>
+                <h1>Increment/Decrement Value</h1>
+                <div>
+                    <button onClick={this.decrementNumber}>-</button>
+                    <input type="text" value={this.state.number} onChange={this.handleInputChange}/>
+                    <button onClick={this.incrementNumber}>+</button>
+                </div>
+                <h5>{this.state.number}</h5>
+
+                <form>
+                    <div>
+                        <label>Full Name</label>
+                        <br/>
+                        <input type="text" name="fullname" onChange={this.handleFormInputChange}/>
+                    </div>
+                    <div>
+                        <label>Email Address</label>
+                        <br/>
+                        <input type="email" name="email" onChange={this.handleFormInputChange}/>
+                    </div>
+                    <div>
+                        <label>Post Code</label>
+                        <br/>
+                        <input type="number" name="postcode" onChange={this.handleFormInputChange}/>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+
             </div>
         );
    }
