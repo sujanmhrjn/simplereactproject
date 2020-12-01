7bcf9c3c88288a6770da747b2d2894c0d0ea225e
Sujan <sujan.maharjan75@gmail.com>
2020-12-01 22:23:19 +0545

    Form Validation added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,19 +7,22 @@ class HomePage extends Component{
             number:2,
             fullname:"",
             email:"",
-            postcode:""
+            errors:{}
+     
         }
         // this.incrementNumber=this.incrementNumber.bind(this);
     }
     incrementNumber=()=> {
-        this.setState({
-            number: ++this.state.number
-        });
+       
+        this.setState(state=>({
+            number: ++state.number
+        }));
     }
     decrementNumber=()=> {
-        this.setState({
-            number:  --this.state.number
-        });
+   
+        this.setState(state=>({
+            number: --state.number
+        }));
     }
     handleInputChange=(e)=>{
         this.setState({
@@ -27,12 +30,30 @@ class HomePage extends Component{
         })
     }
 
-    handleFormInputChange= (e) =>{
+    handleInputChange= (e) =>{
+     this.setState({
+         [e.target.name]:e.target.value
+     })
+    }
+    handleFormValidation = () => {
+        let formError = {};
+        if(!this.state.fullname) formError.fullname="Enter Fullname";
+        if(!this.state.email) formError.email="Enter email";
+        
+        return formError;
+    
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = this.handleFormValidation();
         this.setState({
-            [e.target.name]:e.target.value
+            errors
         })
+        // alert("submitted");
+     
     }
 
+    
 
    render(){
        return (
@@ -45,22 +66,18 @@ class HomePage extends Component{
                 </div>
                 <h5>{this.state.number}</h5>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <label>Full Name</label>
                         <br/>
-                        <input type="text" name="fullname" onChange={this.handleFormInputChange}/>
+                        <input type="text" name="fullname" onChange={this.handleInputChange}/>
                     </div>
                     <div>
                         <label>Email Address</label>
                         <br/>
-                        <input type="email" name="email" onChange={this.handleFormInputChange}/>
+                        <input type="text" name="email" onChange={this.handleInputChange}/>
                     </div>
-                    <div>
-                        <label>Post Code</label>
-                        <br/>
-                        <input type="number" name="postcode" onChange={this.handleFormInputChange}/>
-                    </div>
+                  
                     <button type="submit">Submit</button>
                 </form>
 
